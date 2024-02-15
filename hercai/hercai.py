bc69ded0e5149485a38771de1988a9ff55e3a213
Five So Beş <89789101+Bes-js@users.noreply.github.com>
2024-02-15 14:57:25 +0300

    New Model's

--- hercai/hercai.py
+++ hercai/hercai.py
@@ -7,7 +7,7 @@ class Hercai:
         else:
             self.api_key = api_key
  
-    def question(self, model="v3", content=None, personality=""):
+    def question(self, model="v3", content=None):
         if model not in ["v3", "gemini", "v3-32k", "turbo", "turbo-16k"]:
             model = "v3"
         if not content:
@@ -17,7 +17,6 @@ class Hercai:
             api = requests.get(
                 f"https://hercai.onrender.com/{model}/hercai?question=" + requests.utils.quote(content),
                 headers={"content-type": "application/json","Authorization": self.api_key},
-                data={"personality":personality}
             )
             return api.json()
         except Exception as err:
@@ -37,6 +36,59 @@ class Hercai:
                 + requests.utils.quote(prompt)
                 + "&negative_prompt="
                 + requests.utils.quote(negative_prompt),
+                headers={"content-type": "application/json","Authorization": self.api_key},
+                data={"key": self.api_key},
+            )
+            return api.json()
+        except Exception as err:
+            raise ValueError("Error: " + str(err))
+        
+
+    def beta_question(self,content=None,user=None):
+        if not user:
+            user = ""
+        if not content:
+            raise ValueError("Please specify a question!")
+
+        try:
+            api = requests.get(
+                f"https://hercai.onrender.com/beta/hercai?question=" + requests.utils.quote(content)+"&user="+requests.utils.quote(user),
+                headers={"content-type": "application/json","Authorization": self.api_key},
+            )
+            return api.json()
+        except Exception as err:
+            raise ValueError("Error: " + str(err))
+
+
+
+    def beta_draw_image(self,prompt=None, negative_prompt=None,sampler="DPM-Solver",image_style="Null",width=1024,height=1024,steps=20,scale=5):
+        if sampler not in ["DPM-Solver","SA-Solver"]:
+            sampler = "DPM-Solver"
+        if image_style not in ["Cinematic" , "Photographic" , "Anime" , "Manga" , "Digital Art" , "Pixel art" , "Fantasy art" , "Neonpunk" , "3D Model" , "Null"]:
+            image_style = "Null"
+        if not prompt:
+            raise ValueError("Please specify a prompt!")
+        if not negative_prompt:
+            negative_prompt = ""
+
+        try:
+            api = requests.get(
+                f"https://hercai.onrender.com/beta/text2image?prompt="
+                + requests.utils.quote(prompt)
+                + "&negative_prompt="
+                + requests.utils.quote(negative_prompt)
+                + "&sampler="
+                + requests.utils.quote(sampler)
+                + "&image_style="
+                + requests.utils.quote(image_style)
+                + "&width="
+                + width
+                + "&height="
+                + height
+                + "&steps="
+                + steps
+                + "&scale="
+                + scale,
                 headers={"content-type": "application/json","Authorization": self.api_key},
                 data={"key": self.api_key},
             )
