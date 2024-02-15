b59f2243fd60c2e31880b12c25cfe692bd3a1647
Five So Beş <89789101+Bes-js@users.noreply.github.com>
2024-02-15 14:57:50 +0300

    Version 3.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hercai",
-    version="3.1.0",
+    version="3.2.0",
     author="fivesobes",
     description="A powerful Python Package for interacting with the Herc.ai API.",
     fullname="hercai",
